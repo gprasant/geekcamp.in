836589f715d2186f36bb31d1c2fcfdc4d1701b71
Srikanth <sri.v@thoughtworks.com>
2012-12-02 20:50:38 +0530

    changed connection string to point to cloudbird
    yet to extract to a config - need to identify a mechanism

--- geekcamp/geekcamp.core/Models/ContactRepository.fs
+++ geekcamp/geekcamp.core/Models/ContactRepository.fs
@@ -5,10 +5,12 @@ module ContactRepository =
     open Raven.Client.Document
     open geekcamp.core.Models
     open Raven.Client.Linq
+    open System.Configuration
 
     let newSession =
-        let store = new DocumentStore()
-        store.Url <- "http://localhost:8080/"
+        let store = new DocumentStore() 
+        store.Url <- "https://ec2-eu1.cloudbird.net/Databases/6ecd2066-08c9-4600-9c2b-90593977d91e.geekcamp.in"
+        store.ApiKey <- "ce150dde-6a06-4496-904d-5113d68ef8aa" 
         store.Initialize() |> ignore
         store.OpenSession()
 
